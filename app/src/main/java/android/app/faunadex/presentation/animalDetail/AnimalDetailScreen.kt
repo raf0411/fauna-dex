8ee589e76c3d0f7549b75087d2fd52c9c9f9a030
Raffi <raffidarmawan0411@gmail.com>
2026-01-13 23:59:06 +0700

    Add AnimalDetailScreen composable for animal detail display

--- app/src/main/java/android/app/faunadex/presentation/animalDetail/AnimalDetailScreen.kt
+++ app/src/main/java/android/app/faunadex/presentation/animalDetail/AnimalDetailScreen.kt
@@ -1,8 +1,18 @@
 package android.app.faunadex.presentation.animalDetail
 
+import android.app.faunadex.ui.theme.FaunaDexTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun AnimalDetailScreen() {
+ 
+}
 
+@Preview(showBackground = true)
+@Composable
+fun AnimalDetailScreenPreview() {
+    FaunaDexTheme {
+        AnimalDetailScreen()
+    }
 }
